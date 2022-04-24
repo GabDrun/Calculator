1475c4a5a23284c3a7aa83d269e08a8128f842cc
GabDrun <gabrielius.drungilas@gmail.com>
2022-04-24 22:17:01 +0300

    Initial commit

---
+++ src/pack/Logic.java
@@ -0,0 +1,11 @@
+package pack;
+
+public class Logic {
+    public Logic() {
+    }
+
+    public String calculate(String input) {
+        System.out.println("input: " + input);
+        return "answer";
+    }
+}
